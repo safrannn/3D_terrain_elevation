6c23ecdbc571c5953b9a5f1528dee04e7d2be9a8
Cheng Su <airfluting@Gmail.com>
2019-11-17 22:39:14 -0800

    map

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three';
 const OrbitControls = require('three-orbitcontrols')
 
-const elevationScale = 0.000434 * (4372 - 176)
-const scale = 5
 
 
 var camera, controls, scene, renderer
 var planeSize, plane
+var heightLimit = 4272
+var elevationScale = 0.0316 * heightLimit
+var contourScale = 10
+var contourHeight = 500
 
 // setup scene
 scene = new THREE.Scene();
@@ -24,77 +26,110 @@ camera.lookAt( scene.position )
 controls.enableDamping = true
 controls.dampingFactor = 0.25
 
-
 //add directional light
 var directionalLight = new THREE.DirectionalLight( 0xFFFFFF, 1 );
 directionalLight.position.set( 100, 350, 250 );
 directionalLight.castShadow = true;
 scene.add( directionalLight );
 
+//load image
+var img = new Image()
+img.src = "src/height_map.png"
+
 //generate terrain
 generateTerrain()
 
+//show image
 function animate(){
   renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 }
-
 animate()
 
+//read image pixel information
+function readImageData(){
+  var canvas = document.createElement( 'canvas' )
+  canvas.width = img.width
+  canvas.height = img.height
+  var context = canvas.getContext( '2d' )
+
+  context.drawImage(img,0,0)
+
+  var imgd = context.getImageData(0, 0, img.width, img.height)
+  var pix = imgd.data
+
+  return pix
+}
 
 //return array with height data from img
-function getHeightData(img) {
-  var canvas = document.createElement( 'canvas' );
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var context = canvas.getContext( '2d' );
+function getHeightData() {
+  var pix  = readImageData()
+  var size = img.width * img.height
+  var data = new Float32Array(size)
 
-  var size = img.width * img.height;
-  var data = new Float32Array( size );
-
-  context.drawImage(img,0,0);
-
-  for ( var i = 0; i < size; i ++ ) {
-      data[i] = 0
+  for (var i = 0; i < pix.length; i +=4) {
+      data[i/4] = pix[i]
   }
-
-  var imgd = context.getImageData(0, 0, img.width, img.height);
-  var pix = imgd.data;
-
-  var j=0;
-  for (var i = 0; i<pix.length; i +=4) {
-      var all = pix[i]+pix[i+1]+pix[i+2];
-      data[j++] = all/12;
-  }
-
   return data
 }
 
-function getTexture(img){
+function getTexture(){
+  var pix  = readImageData()
+  var size = img.width * img.height
+  var data = new Uint8Array(img.width * img.height*4)
 
+  for (var i = 0; i < pix.length; i+=4) {
+    // var currentHeight = heightLimit * pix[i] / 256
+    // // console.log(currentHeight)
+    // if (currentHeight % contourHeight < 10){
+    //   data[i] = 0
+    //   data[i+1] = 0
+    //   data[i+2] = 0
+    //   data[i+3] = 255
+    // }else{
+    //   data[i] = 255
+    //   data[i+1] = 255
+    //   data[i+2] = 255
+    //   data[i+3] = 255
+    // }
+    if (pix[i] % 50 == 0){
+      data[i] = 0
+        data[i+1] = 0
+        data[i+2] = 0
+        data[i+3] = 255
+      }else{
+        data[i] = 255
+        data[i+1] = 255
+        data[i+2] = 255
+        data[i+3] = 255
+      }
+  }
+  return data
 }
+
 function generateTerrain(){
   // generate terrain
-  var img = new Image()
-
-  img.src = "src/height_map.png"
-
   img.onload = function () {
     //get height data from img
-    var data = getHeightData(img)
+    var heightData = getHeightData()
 
     planeSize = img.width
+    elevationScale /= (img.width * 0.25)
 
     // plane
-    var geometry = new THREE.PlaneGeometry(planeSize,planeSize,planeSize- 1 ,planeSize - 1);
-    var diffuseMap = new THREE.TextureLoader().load( img.src );
-    var normalMap = new THREE.TextureLoader().load("src/normal_map.png");
-    var material = new THREE.MeshLambertMaterial( {color:diffuseMap, normalMap: normalMap, wireframe: true } );
-    plane = new THREE.Mesh( geometry, material );
+    var geometry = new THREE.PlaneGeometry(planeSize,planeSize,planeSize - 1 ,planeSize - 1)
+    var diffuseMap = new THREE.DataTexture(getTexture(), img.width, img.height, THREE.RGBAFormat )
+    diffuseMap.flipY = true
+    // var normalMap = new THREE.TextureLoader().load("src/normal_map.png")
+    var specularMap = new THREE.TextureLoader().load("src/specular_map.png")
+    var ambientMap = new THREE.TextureLoader().load("src/ambient_map.png")
+    // var material = new THREE.MeshStandardMaterial( {map:diffuseMap, normalMap:normalMap, specularMap:specularMap,aoMap: ambientMap, wireframe: false } )
+    var material = new THREE.MeshStandardMaterial({map:diffuseMap,specularMap:specularMap,aoMap: ambientMap})
+    plane = new THREE.Mesh( geometry, material )
 
     //set height of vertices
     for ( var i = 0; i<plane.geometry.vertices.length; i++ ) {
-         plane.geometry.vertices[i].z = data[i] * elevationScale
+         plane.geometry.vertices[i].z = heightData[i] * elevationScale
     }
 
     plane.geometry.computeVertexNormals();
@@ -102,5 +137,5 @@ function generateTerrain(){
     plane.rotateX(THREE.Math.degToRad(-90))
     scene.add(plane);
     camera.position.z = planeSize * 1.5
-  };
+  }
 }
